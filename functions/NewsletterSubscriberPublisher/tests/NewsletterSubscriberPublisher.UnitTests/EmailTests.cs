a430355374dc266b9f7aae9a1415e2c1fba498f0
Daniel Szopa <daniel.szopa333@gmail.com>
2024-06-17 07:43:55 +0200

    Add unit tests for email model

---
+++ functions/NewsletterSubscriberPublisher/tests/NewsletterSubscriberPublisher.UnitTests/EmailTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NewsletterSubscriberPublisher.Exceptions;
+using NewsletterSubscriberPublisher.Models;
+
+namespace NewsletterSubscriberPublisher.UnitTests;
+
+public class EmailTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    [InlineData("testgmail.com")]
+    [InlineData("test@gmail.gmail.com")]
+    public void Create_WhenValueInputIsInvalid_ShouldThrowInvalidEmailException(string? email)
+    {
+        //act
+        var result = () => { new Email(email); };
+
+        //assert
+        result.Should().Throw<InvalidEmailException>();
+    }
+
+    [Theory]
+    [InlineData("test@gmail.com")]
+    [InlineData("test123@gmail.com")]
+    public void Create_WhenValueInputIsValid_ShouldNotThrowInvalidEmailException(string email)
+    {
+        //act
+        var result = () => { new Email(email); };
+
+        //assert
+        result.Should().NotThrow<InvalidEmailException>();
+    }
+}
